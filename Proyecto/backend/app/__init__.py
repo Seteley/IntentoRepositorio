20c3984db0ea249ec68cfcd2fb7f609c16f8e2f4
Seteley <tejedasergio2004@gmail.com>
2024-11-24 08:05:38 -0500

    a

--- Proyecto/backend/app/__init__.py
+++ Proyecto/backend/app/__init__.py
@@ -7,6 +7,6 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object("app.config.Config")
     app.register_blueprint(router)
-    CORS(app, origins=["https://intentorepositorio.onrender.com"])
+    CORS(app, origins=["https://intentorepositorio.onrender.com"], methods=["GET", "POST", "PUT", "DELETE"], allow_headers=["Content-Type"])
     #CORS(app, origins=["http://localhost:5173"])
     return app
